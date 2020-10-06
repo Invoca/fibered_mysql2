1be4b01d3158c2513f0d753ca22f89b740afc98b
Drew Caddell <dcaddell@invoca.com>
2020-10-06 16:01:24 -0700

    TECH-4819: Add frozen_string_literal magic comment to top of new files

--- lib/fibered_mysql2.rb
+++ lib/fibered_mysql2.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "fibered_mysql2/version"
 
 module FiberedMysql2
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "bundler/setup"
 require "fibered_mysql2"
 require 'rails'
