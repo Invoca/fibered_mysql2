f5cf2f21978624c0dd42893da10b9078f8286ec1
Drew Caddell <dcaddell@invoca.com>
2020-10-12 21:10:50 -0700

    TECH-4819: DRY up setup/teardown of FiberedDatabaseConnectionPool tests

--- spec/unit/fibered_database_connection_pool_spec.rb
+++ spec/unit/fibered_database_connection_pool_spec.rb
@@ -369,40 +369,45 @@ describe FiberedMysql2::FiberedDatabaseConnectionPool do
 
   context ActiveRecord::ConnectionAdapters::ConnectionPool do
     let(:client) { double(Mysql2::EM::Client) }
+    let(:pool_size) { 10 }
+    let(:establish_connection) do
+      ActiveRecord::Base.establish_connection(
+        :adapter => 'fibered_mysql2',
+        :database => 'widgets',
+        :username => 'root',
+        :pool => pool_size
+      )
+    end
+
+    before :each do
+      allow(client).to receive(:query_options) { {} }
+      allow(client).to receive(:escape) { |query| query }
+      allow(client).to receive(:ping) { true }
+      allow(client).to receive(:close)
+      allow(client).to receive(:info).and_return({ version: "5.7.27" })
+      allow(client).to receive(:server_info).and_return({ version: "5.7.27" })
+      allow(Mysql2::EM::Client).to receive(:new) { |config| client }
+
+      establish_connection
+    end
+
+    after :each do
+      EM.run do
+        f = Fiber.new { ActiveRecord::Base.remove_connection }
+        f.resume
+        EM.stop
+      end
+    end
 
     context "with more than 1 connection in the pool" do
-      before :each do
-        ActiveRecord::Base.establish_connection(
-          :adapter => 'fibered_mysql2',
-          :database => 'widgets',
-          :username => 'root',
-          :pool => 10
-        )
-        allow(client).to receive(:query_options) { {} }
-        allow(client).to receive(:escape) { |query| query }
-        allow(client).to receive(:ping) { true }
-        allow(client).to receive(:close)
-        allow(client).to receive(:info).and_return({ version: "5.7.27" })
-        allow(client).to receive(:server_info).and_return({ version: "5.7.27" })
-        allow(Mysql2::EM::Client).to receive(:new) { |config| client }
-      end
-
-      after :each do
-        EM.run do
-          f = Fiber.new { ActiveRecord::Base.remove_connection }
-          f.resume
-          EM.stop
-        end
-      end
-
       it "should serve separate connections per fiber" do
-        version_specific_expectation = if Rails::VERSION::MAJOR > 4
-                                         "SET  @@SESSION.sql_mode = CONCAT(CONCAT(@@sql_mode, ',STRICT_ALL_TABLES'), ',NO_AUTO_VALUE_ON_ZERO'),  @@SESSION.sql_auto_is_null = 0, @@SESSION.wait_timeout = 2147483"
-                                       else
-                                         "SET  @@SESSION.sql_auto_is_null = 0, @@SESSION.wait_timeout = 2147483, @@SESSION.sql_mode = 'STRICT_ALL_TABLES'"
-                                       end
+        expected_query = if Rails::VERSION::MAJOR > 4
+                           "SET  @@SESSION.sql_mode = CONCAT(CONCAT(@@sql_mode, ',STRICT_ALL_TABLES'), ',NO_AUTO_VALUE_ON_ZERO'),  @@SESSION.sql_auto_is_null = 0, @@SESSION.wait_timeout = 2147483"
+                         else
+                           "SET  @@SESSION.sql_auto_is_null = 0, @@SESSION.wait_timeout = 2147483, @@SESSION.sql_mode = 'STRICT_ALL_TABLES'"
+                         end
         expect(client).to receive(:query) do |*args|
-          expect(args).to eq([version_specific_expectation])
+          expect(args).to eq([expected_query])
         end.exactly(2).times
 
         c0 = ActiveRecord::Base.connection
@@ -443,29 +448,10 @@ describe FiberedMysql2::FiberedDatabaseConnectionPool do
     end
 
     context "with only 1 connection in the pool" do
-      before :each do
-        ActiveRecord::Base.establish_connection(
-          :adapter => 'fibered_mysql2',
-          :database => 'widgets',
-          :username => 'root',
-          :pool => 1
-        )
-        allow(client).to receive(:query_options) { {} }
-        allow(client).to receive(:escape) { |query| query }
-        allow(client).to receive(:ping) { true }
-        allow(client).to receive(:close)
-        allow(client).to receive(:info).and_return({ version: "5.7.27" })
-        allow(client).to receive(:server_info).and_return({ version: "5.7.27" })
-        allow(Mysql2::EM::Client).to receive(:new) { |config| client }
-      end
+      let(:pool_size) { 1 }
 
       after :each do
-        ActiveRecord::Base.connection
-        EM.run do
-          f = Fiber.new { ActiveRecord::Base.remove_connection }
-          f.resume
-          EM.stop
-        end
+        ActiveRecord::Base.connection # Reset owner before calling remove_connection
       end
 
       it "should hand off connection on checkin to any fiber waiting on checkout" do
