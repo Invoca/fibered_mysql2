384017fb54e4817226db0c320240caac8600d8e3
Drew Caddell <dcaddell@invoca.com>
2020-10-15 15:03:08 -0700

    TECH-4819: Make subjects in fibered_mysql2_adapter_spec.rb more specific to the contexts they are in

--- spec/unit/fibered_mysql2_adapter_spec.rb
+++ spec/unit/fibered_mysql2_adapter_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe FiberedMysql2::FiberedMysql2Adapter do
   let(:logger) { Logger.new(STDOUT) }
   let(:options) { [] }
   let(:config) { {} }
+  let(:adapter) { FiberedMysql2::FiberedMysql2Adapter.new(client, logger, options, config) }
 
-  subject { FiberedMysql2::FiberedMysql2Adapter.new(client, logger, options, config) }
+  subject { adapter }
 
   before do
     allow(client).to receive(:query_options) { {} }
@@ -22,32 +23,22 @@ RSpec.describe FiberedMysql2::FiberedMysql2Adapter do
     allow(client).to receive(:server_info).and_return({ version: "5.7.27" })
   end
 
-  it 'returns a FiberedMysql2Adapter' do
-    expect(subject).to be_instance_of(FiberedMysql2::FiberedMysql2Adapter)
-  end
+  it { should be_instance_of(FiberedMysql2::FiberedMysql2Adapter) }
 
   context '#lease' do
-    it 'returns the current Fiber if the connection is not in use' do
-      expected_owner = Fiber.current
-      expect(subject.lease).to eq(expected_owner)
-    end
+    subject { adapter.lease }
 
-    case Rails::VERSION::MAJOR
-    when 4
-      it 'returns nil when the connection is in use' do
-        subject.lease
-        expect(subject.lease).to be_nil
-      end
-    when 5, 6
+    it { should eq(Fiber.current) }
+
+    if Rails::VERSION::MAJOR > 4
       it 'raises if connection is already used by the current Fiber' do
-        subject.lease
-        expect{ subject.lease }.to raise_exception(ActiveRecord::ActiveRecordError, "Cannot lease connection, it is already leased by the current fiber.")
+        adapter.lease
+        expect{ subject }.to raise_exception(ActiveRecord::ActiveRecordError, "Cannot lease connection, it is already leased by the current fiber.")
       end
 
-      it 'raises is the connection is used by another Fiber' do
-        subject.lease
-
-        new_fiber = Fiber.new { subject.lease }
+      it 'raises if the connection is used by another Fiber' do
+        adapter.lease
+        new_fiber = Fiber.new { subject }
         expect{ new_fiber.resume }.to raise_exception(ActiveRecord::ActiveRecordError, /Cannot lease connection, it is already in use by a different fiber/)
       end
     end
@@ -55,19 +46,21 @@ RSpec.describe FiberedMysql2::FiberedMysql2Adapter do
 
   if Rails::VERSION::MAJOR > 4
     context '#expire' do
+      subject { adapter.expire }
+
       it 'sets the connection @owner to nil when in use' do
-        subject.lease
-        expect(subject.expire).to be_nil
+        adapter.lease
+        expect(subject).to be_nil
       end
 
       it 'raises if the connection is not in use' do
-        expect{ subject.expire }.to raise_exception(ActiveRecord::ActiveRecordError, "Cannot expire connection, it is not currently leased.")
+        expect{ subject }.to raise_exception(ActiveRecord::ActiveRecordError, "Cannot expire connection, it is not currently leased.")
       end
 
       it 'raises if the connection is owned by a different Fiber' do
-        subject.lease
+        adapter.lease
 
-        new_fiber = Fiber.new { subject.expire }
+        new_fiber = Fiber.new { subject }
         expect{ new_fiber.resume }.to raise_exception(ActiveRecord::ActiveRecordError, /Cannot expire connection.+it is owned by a different fiber/)
       end
     end
