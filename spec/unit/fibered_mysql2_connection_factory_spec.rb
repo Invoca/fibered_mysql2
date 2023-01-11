c077909229a6e00b5b23b3317272ce3057cfd1db
James Ebentier <jebentier@invoca.com>
2023-01-11 12:51:51 +0100

    TECH-9819: refactor existing transactions test before adding lazy transaction support

--- spec/unit/fibered_mysql2_connection_factory_spec.rb
+++ spec/unit/fibered_mysql2_connection_factory_spec.rb
@@ -4,8 +4,11 @@ require_relative '../../lib/fibered_mysql2/fibered_mysql2_connection_factory'
 
 RSpec.describe FiberedMysql2::FiberedMysql2ConnectionFactory do
   let(:client) { double(Mysql2::EM::Client) }
-  
+  let(:stub_mysql_client_result) { Struct.new(:fields, :to_a).new([], []) }
+  let(:connection) { ActiveRecord::Base.connection }
+
   before do
+    allow(client).to receive(:query).and_return(stub_mysql_client_result)
     ActiveRecord::Base.establish_connection(
       :adapter => 'fibered_mysql2',
       :database => 'widgets',
@@ -15,40 +18,27 @@ RSpec.describe FiberedMysql2::FiberedMysql2ConnectionFactory do
   end
 
   context "transactions" do
-    it "should work with basic nesting" do
+    before do
       allow(client).to receive(:query_options) { {} }
       allow(client).to receive(:escape) { |query| query }
-      query_args = []
-      stub_mysql_client_result = Struct.new(:fields, :to_a).new([], [])
-      expect(client).to receive(:query) do |*args|
-        query_args << args
-        stub_mysql_client_result
-      end.at_least(1).times
       allow(client).to receive(:ping) { true }
       allow(client).to receive(:server_info).and_return({ version: "5.7.27" })
-
       allow(Mysql2::EM::Client).to receive(:new) { |config| client }
+    end
 
-      connection = ActiveRecord::Base.connection
+    it "should work with basic nesting" do
       if Rails::VERSION::MAJOR == 6
         allow(connection).to receive(:supports_lazy_transactions?).and_return(false)
       end
 
+
+      expect(client).to receive(:query).with("BEGIN").and_return(stub_mysql_client_result)
+      expect(client).to receive(:query).with("show tables").and_return(stub_mysql_client_result)
+      expect(client).to receive(:query).with("COMMIT").and_return(stub_mysql_client_result)
+
       connection.transaction do
         connection.exec_query("show tables")
       end
-      rails_specific_arg = case Rails::VERSION::MAJOR
-                           when 4
-                             "SET  @@SESSION.sql_auto_is_null = 0, @@SESSION.wait_timeout = 2147483, @@SESSION.sql_mode = 'STRICT_ALL_TABLES'"
-                           else
-                             "SET  @@SESSION.sql_mode = CONCAT(CONCAT(@@sql_mode, ',STRICT_ALL_TABLES'), ',NO_AUTO_VALUE_ON_ZERO'),  @@SESSION.sql_auto_is_null = 0, @@SESSION.wait_timeout = 2147483"
-                           end
-      expect(query_args).to eq([
-                                   [rails_specific_arg],
-                                   ["BEGIN"],
-                                   ["show tables"],
-                                   ["COMMIT"]
-                               ])
     end
   end
 end
